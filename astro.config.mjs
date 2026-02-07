098f45b69643c3ee7b6586938cccf8633f955a19
Logan Rose <logprose@umich.edu>
2026-02-06 22:26:08 -0500

    feat: .github actions to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://loganrose.dev',
+  site: 'https://logancodes.com',
   output: 'static',
   vite: {
     plugins: [tailwindcss()]
